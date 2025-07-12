1624680485f6fe014944488ab249fef979eb0381
tomatoCoderq <xasanFN@mail.ru>
2025-07-12 10:13:11 +0300

    fix: changed project structure

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'layout/app_shell.dart';
+import 'theme.dart';
+import 'util.dart';
+
+class AnkiApp extends StatelessWidget {
+  const AnkiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = createTextTheme(context, 'Archivo', 'Archivo Black');
+
+    return MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      theme: MaterialTheme(textTheme).light(),
+      darkTheme: MaterialTheme(textTheme).dark(),
+      themeMode: ThemeMode.system,
+      home: const AppShell(),
+    );
+  }
+}
